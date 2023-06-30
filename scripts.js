1fec70fcff597f7362b29d8e898e4712d446e0d1
jbprojectmanager <138091265+jbprojectmanager@users.noreply.github.com>
2023-06-30 01:56:21 -0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.menu ul');
-
-hamburger.addEventListener('click', () => {
-  menu.classList.toggle('show');
-});
